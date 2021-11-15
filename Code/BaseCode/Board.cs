7292f8f94b5ca48ccf3e9777c0464b33925d4a78
varat2542 <91092070+varat2542@users.noreply.github.com>
2021-11-15 16:16:03 -0500

    Add files via upload

---
+++ Code/BaseCode/Board.cs
@@ -0,0 +1,277 @@
+﻿#pragma warning disable 1591
+namespace Quadris {
+  public enum CellState {
+    EMPTY,
+    OCCUPIED_PREVIOUSLY,
+    OCCUPIED_ACTIVE_PIECE,
+    COLLISION
+  }
+
+  public enum MoveDir {
+    LEFT,
+    RIGHT,
+    DOWN
+  }
+
+  public class GridCellInfo {
+    public PieceColor Color { get; set; }
+    public CellState State { get; set; }
+
+    public GridCellInfo() {
+      Reset();
+    }
+
+    public void Reset() {
+      Color = PieceColor.NONE;
+      State = CellState.EMPTY;
+    }
+
+    public void SetToActivePiece(Piece activePiece) {
+      State = CellState.OCCUPIED_ACTIVE_PIECE;
+      Color = activePiece.Color;
+    }
+  }
+
+    public class Board {
+        public bool gamegoing = true;
+        public GridCellInfo[,] Grid { get; private set; }
+        public Piece ActivePiece { get; set; }
+
+        public Board() {
+            Grid = new GridCellInfo[24, 10];
+            for (int i = 0; i < Grid.GetLength(0); i++) {
+                for (int j = 0; j < Grid.GetLength(1); j++) {
+                    Grid[i, j] = new GridCellInfo();
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method updates the grid and moves the active piece down
+        /// </summary>
+
+
+        public void Update() {
+            if (gamegoing == true) {
+                if (ActivePieceCanMove(MoveDir.DOWN)) {
+                    ActivePiece.MoveDown();
+                    RefreshGridWithActivePiece();
+                }
+                else {
+                    SettlePiece();
+                    CheckForLine();
+                    checkforgamened();
+                }
+            }
+        }
+
+        public void PauseGame() {
+            if (gamegoing == true) {
+                gamegoing = false;
+            }
+            else {
+                gamegoing = true;
+            }
+        }
+
+        public int ChangePalette(int colorin) {
+            int colorout = colorin + 1;
+            clearboard();
+            return colorout;
+        }
+
+        public void clearboard() {
+            for (int r = 0; r < Grid.GetLength(0); r++) {
+                for (int c = 0; c < Grid.GetLength(1); c++) {
+                    GridCellInfo cellInfo = Grid[r, c];
+                    cellInfo.Reset();
+                    ActivePiece = Piece.GetRandPiece();
+                }
+            }
+        }
+
+        public void checkforgamened() {
+            for (int curRow = 0; curRow < Grid.GetLength(0); curRow++) {
+                for (int col = 1; col < Grid.GetLength(1); col++) {
+                    GridCellInfo cellInfo = Grid[curRow, col];
+                    if (GetCellInfo(3, col)?.State == CellState.OCCUPIED_PREVIOUSLY) {
+                        PauseGame();
+                        clearboard();
+                    }
+                }
+            }
+        }
+
+        private void RefreshGridWithActivePiece() {
+      for (int r = 0; r < Grid.GetLength(0); r++) {
+        for (int c = 0; c < Grid.GetLength(1); c++) {
+          GridCellInfo cellInfo = Grid[r, c];
+          if (cellInfo.State == CellState.OCCUPIED_ACTIVE_PIECE) {
+            cellInfo.Reset();
+          }
+        }
+      }
+      for (int r = 0; r < ActivePiece.Layout.GetLength(0); r++) {
+        for (int c = 0; c < ActivePiece.Layout.GetLength(1); c++) {
+          if (ActivePiece.Layout[r, c]) {
+            GridCellInfo cellInfo = GetCellInfo(r + ActivePiece.GridRow, c + ActivePiece.GridCol);
+            cellInfo?.SetToActivePiece(ActivePiece);
+          }
+        }
+      }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="row">the row</param>
+    /// <param name="col">the column</param>
+    /// <returns>A GridCellInfo object</returns>
+    public GridCellInfo GetCellInfo(int row, int col) {
+      if (row < 0 || row >= Grid.GetLength(0) || col < 0 || col >= Grid.GetLength(1))
+        return null;
+      else
+        return Grid[row, col];
+    }
+
+    public void MoveActivePieceLeft() {
+      if (ActivePieceCanMove(MoveDir.LEFT)) {
+        ActivePiece.MoveLeft();
+        RefreshGridWithActivePiece();
+      }
+    }
+
+    public void MoveActivePieceRight() {
+      if (ActivePieceCanMove(MoveDir.RIGHT)) {
+        ActivePiece.MoveRight();
+        RefreshGridWithActivePiece();
+      }
+    }
+
+    public void RotateActivePieceRight() {
+      ActivePiece.RotateRight();
+      if (CheckForOutOfBounds()) {
+        ActivePiece.RotateLeft();
+      }
+    }
+
+    public void RotateActivePieceLeft() {
+      ActivePiece.RotateLeft();
+      if (CheckForOutOfBounds()) {
+        ActivePiece.RotateRight();
+      }
+    }
+
+    public bool ActivePieceCanMove(MoveDir moveDir) {
+      bool canMove = true;
+      switch (moveDir) {
+        case MoveDir.DOWN:
+          for (int c = 0; c < ActivePiece.Layout.GetLength(1); c++) {
+            int lastRow = -1;
+            for (int r = 0; r < ActivePiece.Layout.GetLength(0); r++) {
+              if (ActivePiece.Layout[r, c]) {
+                lastRow = r;
+              }
+            }
+            if (lastRow == -1) {
+              continue;
+            }
+            GridCellInfo cellInfo = GetCellInfo(ActivePiece.GridRow + lastRow + 1, ActivePiece.GridCol + c);
+            if (cellInfo == null || cellInfo.State == CellState.OCCUPIED_PREVIOUSLY) {
+              canMove = false;
+              break;
+            }
+          }
+          break;
+
+        case MoveDir.LEFT:
+          for (int r = 0; r < ActivePiece.Layout.GetLength(0); r++) {
+            int firstCol = -1;
+            for (int c = 0; c < ActivePiece.Layout.GetLength(1); c++) {
+              if (ActivePiece.Layout[r, c]) {
+                firstCol = c;
+                break;
+              }
+            }
+            if (firstCol == -1) {
+              continue;
+            }
+            GridCellInfo cellInfo = GetCellInfo(ActivePiece.GridRow + r, ActivePiece.GridCol + firstCol - 1);
+            if (cellInfo == null || cellInfo.State == CellState.OCCUPIED_PREVIOUSLY) {
+              canMove = false;
+              break;
+            }
+          }
+          break;
+
+        case MoveDir.RIGHT:
+          for (int r = 0; r < ActivePiece.Layout.GetLength(0); r++) {
+            int lastCol = -1;
+            for (int c = 0; c < ActivePiece.Layout.GetLength(1); c++) {
+              if (ActivePiece.Layout[r, c]) {
+                lastCol = c;
+              }
+            }
+            if (lastCol == -1) {
+              continue;
+            }
+            GridCellInfo cellInfo = GetCellInfo(ActivePiece.GridRow + r, ActivePiece.GridCol + lastCol + 1);
+            if (cellInfo == null || cellInfo.State == CellState.OCCUPIED_PREVIOUSLY) {
+              canMove = false;
+              break;
+            }
+          }
+          break;
+      }
+      return canMove;
+    }
+
+    private bool CheckForOutOfBounds() {
+      for (int r = 0; r < ActivePiece.Layout.GetLength(0); r++) {
+        for (int c = 0; c < ActivePiece.Layout.GetLength(1); c++) {
+          if (ActivePiece.Layout[r, c]) {
+            GridCellInfo cellInfo = GetCellInfo(r + ActivePiece.GridRow, c + ActivePiece.GridCol);
+            if (cellInfo == null) {
+              return true;
+            }
+          }
+        }
+      }
+      return false;
+    }
+
+    public void SettlePiece() {
+      for (int r = 0; r < Grid.GetLength(0); r++) {
+        for (int c = 0; c < Grid.GetLength(1); c++) {
+          GridCellInfo cellInfo = Grid[r, c];
+          if (cellInfo.State == CellState.OCCUPIED_ACTIVE_PIECE) {
+            cellInfo.State = CellState.OCCUPIED_PREVIOUSLY;
+          }
+        }
+      }
+      ActivePiece = Piece.GetRandPiece();
+    }
+
+    public void CheckForLine() {
+      for (int curRow = 0; curRow < Grid.GetLength(0); curRow++) {
+        bool allFilled = true;
+        for (int col = 0; col < Grid.GetLength(1); col++) {
+          if (GetCellInfo(curRow, col)?.State == CellState.EMPTY) {
+            allFilled = false;
+            break;
+          }
+        }
+        if (allFilled) {
+          for (int col = 0; col < Grid.GetLength(1); col++) {
+            for (int dropRow = curRow; dropRow > 0; dropRow--) {
+              Grid[dropRow, col] = Grid[dropRow - 1, col];
+               System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\zacha\OneDrive\Desktop\CSC-403\Quadris-main\Code\Quadris\Data\se.wav");
+               player.Play();
+            }
+          }
+          curRow--;
+        }
+      }
+    }
+  }
+}
